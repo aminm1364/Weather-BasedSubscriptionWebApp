f3ea5086452ba42561d99e89db82f7f9a0ceeb1b
Amin Mahmoodian <a.mahmoodiyan@gmail.com>
2025-04-08 16:43:23 -0400

    disabling CORS!!!!!

--- WSWA.Core/Program.cs
+++ WSWA.Core/Program.cs
@@ -8,12 +8,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Adding Database connection string
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=subscriptions.db"));
 
 builder.Services.AddHttpClient<WeatherService>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // TODO: only for development of course!!!! @aminm1364
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +32,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.UseCors();
 app.MapControllers();
 app.Run();
